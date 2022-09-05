b8c4662066a01e16807e3cdac776a6f5b8d5a28f
Viktor Pekin <Viktorpekin43@gmail.com>
2022-09-05 20:13:02 +0700

    delete CORS

--- app.js
+++ app.js
@@ -21,30 +21,6 @@ app.use(bodyParser.json());
 
 app.use(limiter.limiter);
 
-const allowedCors = [
-  'https://movies41.students.nomoredomains.sbs',
-  'http://movies41.students.nomoredomains.sbs',
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  } else {
-    next();
-  }
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
-  }
-
-  next();
-});
-
 mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
