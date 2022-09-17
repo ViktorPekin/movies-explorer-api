27ed718a7b2bbdd2d8c5cc8f99a7f4260b9df1df
ViktorPekin <viktorpekin43@gmail.com>
2022-09-17 13:43:43 +0700

    add: Cross

--- app.js
+++ app.js
@@ -18,6 +18,31 @@ app.use(bodyParser.json());
 
 app.use(requestLogger);
 
+const allowedCors = [
+  'https://movies41.students.nomoredomains.sbs',
+  'http://movies41.students.nomoredomains.sbs',
+  'localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  } else {
+    next();
+  }
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+  }
+
+  next();
+});
+
 app.use(limiter.limiter);
 
 mongoose.connect(NODE_ENV === 'production' ? ADDRESS_DB : 'mongodb://localhost:27017/moviesdb', {
